2bb6bb7b66e48ba0b232e82b4d19a7dd6049d676
UserVanya <general@startev-ivan.ru>
2023-08-31 15:18:04 +0300

    4 lesson passed

--- starproject/starproject/settings.py
+++ starproject/starproject/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = "django-insecure-ut*$(jl09mtwx82r=2vh_5(!iwf&-(e&zz7-3h8)9u3__w8=^x
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
@@ -122,3 +123,7 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+MEDIA_URL = '/media/'
--- starproject/women/views.py
+++ starproject/women/views.py
@@ -1,9 +1,22 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
+from django.http import HttpResponse, HttpResponseNotFound, Http404
+from django.shortcuts import redirect
 # Create your views here.
 
 def index(request): #HttpRequest
     return HttpResponse("Страница women")
-def categories(request):
-    return HttpResponse("<h1>Статьи по категориям<h1>")
+
+def categories(request, cat):
+    if(request.GET):
+        print(request.GET)
+    return HttpResponse(f"<h1>Статьи по категориям </h1> {cat}")
+
+def archive(request, year):
+    if int(year) > 2023:
+        #raise Http404()
+        return redirect('home', permanent=True)
+
+    return HttpResponse(f"<h1>Архив по годам</h1> <p>{year}</p>")
+
+def page_not_found(request, exception):
+    return HttpResponseNotFound("<h1>Страница не найдена</h1>")
