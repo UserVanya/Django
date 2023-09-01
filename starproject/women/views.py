82eb3292c9ad1e42cbbb61dd38a7bba5db490603
UserVanya <general@startev-ivan.ru>
2023-09-01 22:36:01 +0300

    lesson 6 passed

--- starproject/women/views.py
+++ starproject/women/views.py
@@ -1,22 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound, Http404
 from django.shortcuts import redirect
+from .models import *
 # Create your views here.
-
+menu = ["О сайте", "Добавить статью", "Обратная связь", "Войти"]
 def index(request): #HttpRequest
-    return HttpResponse("Страница women")
+    posts = Women.objects.all()
+    return render(request, 'women/index.html', {'posts':posts, 'menu': menu, 'title': 'Главная страница'})
 
-def categories(request, cat):
-    if(request.GET):
-        print(request.GET)
-    return HttpResponse(f"<h1>Статьи по категориям </h1> {cat}")
-
-def archive(request, year):
-    if int(year) > 2023:
-        #raise Http404()
-        return redirect('home', permanent=True)
-
-    return HttpResponse(f"<h1>Архив по годам</h1> <p>{year}</p>")
+def about(request):
+    return render(request, 'women/about.html', {'title': 'О сайте'})
 
 def page_not_found(request, exception):
     return HttpResponseNotFound("<h1>Страница не найдена</h1>")
