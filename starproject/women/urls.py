fe853f660f5cd8bc82b813097987b12138318972
UserVanya <general@startev-ivan.ru>
2023-09-02 15:15:11 +0300

    lesson 9 passed

--- starproject/women/urls.py
+++ starproject/women/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path("login/", login, name='login'),
     path("addpage/", addpage, name='add_page'),
     path("post/<int:post_id>/", show_post, name='post'),
+    path("category/<int:cat_id>/", show_category, name='category'),
 ]
--- starproject/women/views.py
+++ starproject/women/views.py
@@ -10,10 +10,13 @@ menu = [{"title":"О сайте", "url_name":"about"},
  
 def index(request): #HttpRequest
     posts = Women.objects.all()
+    cats = Category.objects.all()
     context = {
         'posts': posts,
+        'cats': cats,
         'menu': menu,
-        'title': 'Главная страница'
+        'title': 'Главная страница',
+        'cat_selected': 0,
     }
     return render(request, 'women/index.html', context=context)
 
@@ -34,3 +37,17 @@ def page_not_found(request, exception):
 
 def show_post(request, post_id):
     return HttpResponse(f"Отображение статьи с id = {post_id}")
+
+def show_category(request, cat_id):
+    posts = Women.objects.filter(cat_id=cat_id)
+    cats = Category.objects.all()
+    if len(posts) == 0:
+        raise Http404()
+    context = {
+        'posts': posts,
+        'cats': cats,
+        'menu': menu,
+        'title': 'Главная страница',
+        'cat_selected': cat_id,
+    }
+    return render(request, 'women/index.html', context=context)
