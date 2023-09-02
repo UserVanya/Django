e624336ac84467f698bbdd15a47d0f1fd2d8e48a
UserVanya <general@startev-ivan.ru>
2023-09-02 16:56:50 +0300

    lesson 11 passed

--- starproject/women/views.py
+++ starproject/women/views.py
@@ -3,18 +3,14 @@ from django.http import HttpResponse, HttpResponseNotFound, Http404
 from django.shortcuts import redirect
 from .models import *
 # Create your views here.
-menu = [{"title":"О сайте", "url_name":"about"},
-        {"title":"Добавить статью", "url_name":"add_page"},
-        {"title":"Обратная связь", "url_name":"contact"},
-        {"title":"Войти", "url_name":"login"}]
- 
+
 def index(request): #HttpRequest
     posts = Women.objects.all()
-    cats = Category.objects.all()
+    #cats = Category.objects.all()
     context = {
         'posts': posts,
-        'cats': cats,
-        'menu': menu,
+    #    'cats': cats,
+    #    'menu': menu,
         'title': 'Главная страница',
         'cat_selected': 0,
     }
@@ -40,13 +36,13 @@ def show_post(request, post_id):
 
 def show_category(request, cat_id):
     posts = Women.objects.filter(cat_id=cat_id)
-    cats = Category.objects.all()
+    #cats = Category.objects.all()
     if len(posts) == 0:
         raise Http404()
     context = {
         'posts': posts,
-        'cats': cats,
-        'menu': menu,
+    #    'cats': cats,
+    #    'menu': menu,
         'title': 'Главная страница',
         'cat_selected': cat_id,
     }
