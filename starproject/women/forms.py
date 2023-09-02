35053da1277679483a34053077111232e472711a
UserVanya <general@startev-ivan.ru>
2023-09-02 19:20:12 +0300

    lesson 14 passed

--- starproject/women/forms.py
+++ starproject/women/forms.py
@@ -1,10 +1,25 @@
+from typing import Any, Dict, Mapping, Optional, Type, Union
 from django import forms
+from django.core.files.base import File
+from django.db.models.base import Model
+from django.forms.utils import ErrorList
 from .models import *
+from django.core.exceptions import ValidationError
 
-class AddPostForm(forms.Form):
-    title = forms.CharField(max_length=255, label="Название", widget=forms.TextInput(attrs={"class": "form-input"}))
-    slug = forms.SlugField(max_length=255, label="URL")
-    content = forms.CharField(widget=forms.Textarea(attrs={"cols": 60, "rows": 10}), label="Контент")
-    is_published = forms.BooleanField(label="Опубликовано", required=False, initial=True)
-    #photo = forms.ImageField(label="Фото", required=False)
-    cat = forms.ModelChoiceField(queryset=Category.objects.all(), label="Категория", empty_label="Категория не выбрана")
+class AddPostForm(forms.ModelForm):
+    def __init__(self, *args, **kwrgs) -> None:
+        super().__init__(*args, **kwrgs)
+        self.fields['cat'].empty_label = "Выберите категорию"
+    class Meta:
+        model = Women
+        fields = ['title', 'slug', 'content', 'photo', 'is_published',  'cat']
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-input', 'placeholder': 'Введите название статьи'}),
+            'content': forms.Textarea(attrs={'cols':100, 'rows':10, 'placeholder': 'Введите текст статьи'}),
+        }
+    
+    def clean_title(self):
+        title = self.cleaned_data['title']
+        if len(title) > 200:
+            raise ValidationError("Длина превышает 200 символов")
+        return title
--- starproject/women/views.py
+++ starproject/women/views.py
@@ -25,14 +25,11 @@ def login(request):
 
 def addpage(request):
     if request.method == 'POST':
-        form = AddPostForm(request.POST)
+        form = AddPostForm(request.POST, request.FILES)
         if form.is_valid():
             #print(form.cleaned_data)
-            try:
-                Women.objects.create(**form.cleaned_data)
-                return redirect('home')
-            except:
-                form.add_error(None, "Ошибка добавления поста")
+            form.save()
+            return redirect('home')
     else:
         form = AddPostForm()
     return render(request, 'women/addpage.html', {'form':form, 'title': 'Добавление статьи'})
