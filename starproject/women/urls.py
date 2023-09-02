18ce2922cafa6f2f48aa227f3d959cedd01d3463
UserVanya <general@startev-ivan.ru>
2023-09-02 09:12:18 +0300

    lesson 8 passed

--- starproject/women/urls.py
+++ starproject/women/urls.py
@@ -3,5 +3,9 @@ from .views import *
 
 urlpatterns = [
     path('', index, name='home'),
-    path("about/", about),
+    path("about/", about, name='about'),
+    path("contact/", contact, name='contact'),
+    path("login/", login, name='login'),
+    path("addpage/", addpage, name='add_page'),
+    path("post/<int:post_id>/", show_post, name='post'),
 ]
--- starproject/women/views.py
+++ starproject/women/views.py
@@ -3,13 +3,34 @@ from django.http import HttpResponse, HttpResponseNotFound, Http404
 from django.shortcuts import redirect
 from .models import *
 # Create your views here.
-menu = ["О сайте", "Добавить статью", "Обратная связь", "Войти"]
+menu = [{"title":"О сайте", "url_name":"about"},
+        {"title":"Добавить статью", "url_name":"add_page"},
+        {"title":"Обратная связь", "url_name":"contact"},
+        {"title":"Войти", "url_name":"login"}]
+ 
 def index(request): #HttpRequest
     posts = Women.objects.all()
-    return render(request, 'women/index.html', {'posts':posts, 'menu': menu, 'title': 'Главная страница'})
+    context = {
+        'posts': posts,
+        'menu': menu,
+        'title': 'Главная страница'
+    }
+    return render(request, 'women/index.html', context=context)
 
 def about(request):
     return render(request, 'women/about.html', {'title': 'О сайте'})
 
+def contact(request):
+    return HttpResponse("Обратная связь")
+
+def login(request):
+    return HttpResponse("Авторизация")
+
+def addpage(request):
+    return HttpResponse("Добавление статьи")
+
 def page_not_found(request, exception):
     return HttpResponseNotFound("<h1>Страница не найдена</h1>")
+
+def show_post(request, post_id):
+    return HttpResponse(f"Отображение статьи с id = {post_id}")
