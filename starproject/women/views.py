4ec347e811a6cb108e081a057fc1c52646ec2979
UserVanya <general@startev-ivan.ru>
2023-09-08 13:51:45 +0300

    lesson 17 passed

--- starproject/women/views.py
+++ starproject/women/views.py
@@ -7,28 +7,22 @@ from django.shortcuts import redirect, get_object_or_404
 from .models import *
 from .forms import AddPostForm
 from django.views.generic import ListView, DetailView, CreateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from .utils import *
 # Create your views here.
 
-class WomenHome(ListView):
+class WomenHome(DataMixin, ListView):
     model = Women
     template_name = 'women/index.html'
     context_object_name = 'posts'
     
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['title'] = 'Главная страница'
-        context['cat_selected'] = 0
-        return context
+        c_def = self.get_user_context(title='Главная страница')
+        return dict(list(context.items()) + list(c_def.items()))
+    
     def get_queryset(self) -> QuerySet[Any]:
         return Women.objects.filter(is_published=True)
-# def index(request): #HttpRequest
-#     posts = Women.objects.all()
-#     context = {
-#         'posts': posts,
-#         'title': 'Главная страница',
-#         'cat_selected': 0,
-#     }
-#     return render(request, 'women/index.html', context=context)
 
 def about(request):
     return render(request, 'women/about.html', {'title': 'О сайте'})
@@ -39,59 +33,34 @@ def contact(request):
 def login(request):
     return HttpResponse("Авторизация")
 
-# def addpage(request):
-#     if request.method == 'POST':
-#         form = AddPostForm(request.POST, request.FILES)
-#         if form.is_valid():
-#             #print(form.cleaned_data)
-#             form.save()
-#             return redirect('home')
-#     else:
-#         form = AddPostForm()
-#     return render(request, 'women/addpage.html', {'form':form, 'title': 'Добавление статьи'})
-class AddPage(CreateView):
+class AddPage(LoginRequiredMixin, DataMixin, CreateView):
     form_class = AddPostForm
     template_name = 'women/addpage.html'
     success_url = reverse_lazy('home')
+    login_url = reverse_lazy('home')
+    raise_exception = True
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['title'] = 'Добавление статьи'
-        return context
+        c_def = self.get_user_context(title='Добавление статьи')
+        return dict(list(context.items()) + list(c_def.items()))
 
 def page_not_found(request, exception):
     return HttpResponseNotFound("<h1>Страница не найдена</h1>")
 
-# def show_post(request, post_slug):
-#     post = get_object_or_404(Women, slug=post_slug)
 
-#     context = {
-#         'post': post,
-#         'title': post.title,
-#         'cat_selected': post.cat_id,
-#     }
-
-#     return render(request, 'women/post.html', context=context)
-class ShowPost(DetailView):
+class ShowPost(DataMixin, DetailView):
     model = Women
     template_name = 'women/post.html'
     context_object_name = 'post'
     slug_url_kwarg = 'post_slug'
 
-
-# def show_category(request, cat_slug):
-#     category = get_object_or_404(Category, slug=cat_slug)
-#     posts = Women.objects.filter(cat_id=category.pk)
-#     #cats = Category.objects.all()
-#     if len(posts) == 0:
-#         raise Http404()
-#     context = {
-#         'posts': posts,
-#         'title': 'Главная страница',
-#         'cat_selected': category.pk,
-#     }
-#     return render(request, 'women/index.html', context=context)
-class WomenCategory(ListView):
+    def get_context_data(self, *, object_list=None, **kwargs: Any) -> dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+        c_def = self.get_user_context(title=context['post'])
+        return dict(list(context.items()) + list(c_def.items()))
+    
+class WomenCategory(DataMixin, ListView):
     model = Women
     template_name = 'women/index.html'
     context_object_name = 'posts'
@@ -102,6 +71,5 @@ class WomenCategory(ListView):
     
     def get_context_data(self, *, object_list=None, **kwargs: Any) -> Dict[str, Any]:
         context = super().get_context_data(**kwargs)
-        context['title'] = 'Категория - ' + str(context['posts'][0].cat)
-        context['cat_selected'] = context['posts'][0].cat_id
-        return context
+        c_def = self.get_user_context(title='Категория - ' + str(context['posts'][0].cat), cat_selected=context['posts'][0].cat_id)
+        return dict(list(context.items()) + list(c_def.items()))
